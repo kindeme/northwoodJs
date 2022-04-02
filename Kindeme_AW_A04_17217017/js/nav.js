d13f9d10cc93f3da39fd9b04e87f3a0c6fde507e
Kindeme KOUNOUDJI <fkounoudji@gmail.com>
2022-04-01 23:14:14 -0500

    expand/collapse started

--- Kindeme_AW_A04_17217017/js/nav.js
+++ Kindeme_AW_A04_17217017/js/nav.js
@@ -1,4 +1,7 @@
 "use strict";
 $(document).ready(() => {
-	$("#nav-toggle").click((evt) => {});
+	$("#nav-toggle").click((evt) => {
+		$("#nav").toggleClass("open");
+		evt.preventDefault();
+	});
 });
